3c506d232aec4fdd58c6c5cc34086a3796f8b8b5
wangchen5 <wangchen5@asiainfo.com>
2018-07-31 11:01:15 +0800

    PRDBD_REQ_20180628_0034,add suggestion parameter in catalog

--- src/main/java/com/asiainfo/bdx/ldp/datafoundry/servicebroker/ocdp/model/CustomizeQuotaItem.java
+++ src/main/java/com/asiainfo/bdx/ldp/datafoundry/servicebroker/ocdp/model/CustomizeQuotaItem.java
@@ -1,33 +1,58 @@
 package com.asiainfo.bdx.ldp.datafoundry.servicebroker.ocdp.model;
 
 import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by baikai on 4/26/17.
  */
-public class CustomizeQuotaItem{
+public class CustomizeQuotaItem {
 
-    @SerializedName("default")
-    long _default;
+	@SerializedName("default")
+	long _default;
 
-    @SerializedName("max")
-    long max;
+	@SerializedName("max")
+	long max;
 
-    @SerializedName("price")
-    long price;
+	@SerializedName("price")
+	long price;
 
-    @SerializedName("unit")
-    String unit;
+	@SerializedName("unit")
+	String unit;
 
-    @SerializedName("step")
-    long step;
+	@SerializedName("step")
+	long step;
 
-    @SerializedName("desc")
-    String desc;
+	@SerializedName("desc")
+	String desc;
 
-    public long getDefault(){ return _default;}
-    public long getMax() { return max; }
-    public long getPrice() { return price; }
-    public String getUnit() { return unit; }
-    public long getStep() { return step; }
-    public String getDesc() { return desc; }
+	@SerializedName("suggestion")
+	String suggestion;
+
+	public String getSuggestion() {
+		return suggestion;
+	}
+
+	public long getDefault() {
+		return _default;
+	}
+
+	public long getMax() {
+		return max;
+	}
+
+	public long getPrice() {
+		return price;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public long getStep() {
+		return step;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
 }
